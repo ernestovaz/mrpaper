1db8f222245d094f13c0d5a18a62b28ee0db9f84
Rafael Trevisan <rafa.trevi@hotmail.com>
2021-05-17 13:09:03 -0300

    aa

--- tcp-mr-paper/src/mrpaper/tests/ArticleTest.java
+++ tcp-mr-paper/src/mrpaper/tests/ArticleTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 
 import mrpaper.business.domain.Article;
 import mrpaper.business.domain.Researcher;
+import mrpaper.business.domain.Review;
 import mrpaper.data.Database;
 
 public class ArticleTest {
@@ -30,15 +31,23 @@ public class ArticleTest {
 	public void validReviewersTest() {
 		List<Researcher> joao =Arrays.asList(database.getResearcherById(1));
 		List<Researcher> ana =Arrays.asList(database.getResearcherById(2));
+		List<Researcher> manoel = Arrays.asList(database.getResearcherById(3));
 		List<Researcher> suzana =Arrays.asList(database.getResearcherById(7));
 		List<Researcher> natasha =Arrays.asList(database.getResearcherById(8));
 		Article articleA = database.getArticleById(1);
 		Article articleB = database.getArticleById(7);
-		assertTrue(articleA.validReviewers(joao).isEmpty());  //Jo�o n�o pode avaliar o pr�prio artigo
+		Article articleC = database.getArticleById(11);
+		assertTrue(articleA.validReviewers(joao).isEmpty());  
 	  	assertFalse(articleA.validReviewers(ana).isEmpty());  
 		assertTrue(articleA.validReviewers(suzana).isEmpty());
 		assertTrue(articleA.validReviewers(natasha).isEmpty());
 		assertFalse(articleB.validReviewers(joao).isEmpty());
+		assertFalse(articleC.validReviewers(manoel).isEmpty());
+		manoel.get(0).includeReview(new Review(12, articleC, manoel.get(0), null));
+		assertFalse(articleC.validReviewers(manoel).isEmpty());
+		articleC.includeReview(new Review(12, articleC, manoel.get(0), null));
+		assertTrue(articleC.validReviewers(manoel).isEmpty());
+		
 	}
 	
 	@Test
